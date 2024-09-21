0f1d2f6e69f45082a57c0f16168826f0ef0042a6
yasmine-hayhay <compte.cm1@gmail.com>
2024-09-21 17:30:40 +0100

    server comments

--- server/Program.cs
+++ server/Program.cs
@@ -6,12 +6,24 @@ using System.Threading.Tasks;
 
 namespace ClientServer
 {
+    /// <summary>
+    /// The Program class contains the entry point of the application.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// The main entry point of the application.
+        /// </summary>
+        /// <param name="args">An array of command-line arguments.</param>
         static void Main(string[] args)
         {
+            // Create an instance of the Server class
             var server = new Server();
+
+            // Start the server and listen for incoming connections
             server.Connect();
+
+            // Wait for the user to press Enter before closing the application
             Console.ReadLine();
         }
     }
